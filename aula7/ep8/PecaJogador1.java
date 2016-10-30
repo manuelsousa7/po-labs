e98b74e844d29a1428a753be0fb5d3e4cd83e0a4
Manuel Sousa <manuelsousamvs@gmail.com>
2016-10-30 12:36:54 +0000

    ep8 init

---
+++ aula7/ep8/PecaJogador1.java
@@ -0,0 +1,19 @@
+package jogogalo;
+
+/**
+ * Classe que representa uma peca do jogador1 do jogo do galo.
+ *
+ * @author Manuel e Goncalo
+ * @version 1.0
+ */
+public class PecaJogador1 extends Peca
+{
+	/**
+     * Construtor da classe.
+     * Atribui "x" ao simbolo da peca e
+     * "Jogador1" ao nome do jogador.
+     */
+	public PecaJogador1() {
+		super("x", "Jogador1");
+	}
+}
